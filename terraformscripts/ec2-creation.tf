248d655d49f45398e31a3cec943405faaee9bf1e
Shouvick Aich <167152865+shouvickaich@users.noreply.github.com>
2024-07-29 16:53:13 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,9 +4,9 @@ region = "ap-south-1"
 
 resource "aws_instance" "AWS_EC2INSTANCE"{
 ami = "ami-068e0f1a600cd311c"
-instance_type = "t2.micro"
-key_name = "devopsmssjuly"
-security_groups = ["launch-wizard-2"]
+instace_type ="t2.micro"
+key_name = "maven-new"
+security_group =["default"]
 tags = {
 Name = "terraformserver"
 }
