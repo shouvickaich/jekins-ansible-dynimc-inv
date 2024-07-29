63a6842ce1a5982d7d32e3d35ebc315720c29d26
Shouvick Aich <167152865+shouvickaich@users.noreply.github.com>
2024-07-29 17:24:11 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "AWS_EC2INSTANCE"{
 ami = "ami-068e0f1a600cd311c"
 instance_type ="t2.micro"
 key_name = "maven-new"
-security_group = ["default"]
+security_groups = ["default"]
 tags = {
 Name = "terraformserver"
 }
